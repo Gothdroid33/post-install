a51355cde8eb528ca900f84af8bd1de64129a745
Sylvain ANGLADE <sylvain.anglade@gmail.com>
2021-01-15 17:08:46 +0100

    refacto post-install.sh
    Add docker for current distribution
    Add zip extension for php
    Refacto identation

--- post-install.sh
+++ post-install.sh
@@ -26,7 +26,7 @@ else
 		do
 		    case $choice in
 	        1)
-	            	#Install VsCode
+	            		#Install Snap
 				echo "Installing Snap for Snapcrafter"
 				apt install snapd -y
 				;;
@@ -36,8 +36,8 @@ else
 				echo "Installing Apache"
 				apt install apache2 -y
 
-        		echo "Installing PHP"
-				apt install php libapache2-mod-php php-mbstring php-dev php-intl php-gd php-pgsql php-sqlite3 php-pear php-mysql -y
+        			echo "Installing PHP"
+				apt install php libapache2-mod-php php-mbstring php-dev php-intl php-gd php-zip php-pgsql php-sqlite3 php-pear php-mysql -y
 				php -v
 				echo "Enabling module rewrite"
 				sudo a2enmod rewrite
@@ -50,9 +50,9 @@ else
 			3)
 				#Install Nodejs
 				echo "Installing Nodejs"
-				curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+				curl -sL https://deb.nodesource.com/setup_15.x | sudo -E bash -
 				apt install -y nodejs
-                npm i -g yarn
+                		npm i -g yarn
 				;;
 
 			4)
@@ -63,37 +63,41 @@ else
 			5)
 				#Composer
 				echo "Installing Composer"
-				apt install composer -y
+				php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+				php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+				php composer-setup.php
+				php -r "unlink('composer-setup.php');"
+				mv composer.phar /usr/local/bin/composer
 				;;
 	
 			6)
 				#Docker
 				echo "Installing Docker"
 				apt remove docker docker-engine docker.io containerd runc -y
-                apt update
-                apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-agent -y
-                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-                add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-                apt update
-                apt install docker-ce docker-ce-cli containerd.io docker-compose -y
-                adduser $SUDO_USER docker
+                		apt update
+                		apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-agent -y
+                		curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+                		add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+                		apt update
+                		apt install docker-ce docker-ce-cli containerd.io docker-compose -y
+                		adduser $SUDO_USER docker
 				;;
-            7)
-                #PhpStorm
-                echo "Installing PhpStorm"
-                snap install phpstorm --classic
-                ;;
-            8) 
-                #VsCode
-                echo "Installing VsCode"
-                snap install code --classic
-                ;;
-            9)
-                #AutoClean && AutoRemove
-                echo "Clean in progress and reboot"
-                apt autoclean -y && apt autoremove -y
-                reboot
-                ;;
+            		7)
+                		#PhpStorm
+                		echo "Installing PhpStorm"
+                		snap install phpstorm --classic
+                		;;
+            		8) 
+                		#VsCode
+                		echo "Installing VsCode"
+                		snap install code --classic
+                		;;
+            		9)
+                		#AutoClean && AutoRemove
+                		echo "Clean in progress and reboot"
+                		apt autoclean -y && apt autoremove -y
+                		reboot
+                		;;
 			
 	    esac
 	done
